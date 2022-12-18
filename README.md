9d333375d57820e3c5f528abbd64d13b5d3eef0e
alireza-hp elitedesk pc <alireza.zabihi@yahoo.com>
2022-12-18 13:37:26 +0330

    style and add readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # calculator
-a calculator!
+
+## [Live Demo](https://arz-barca.github.io/calculator/)
+
+---
+
+[![calculator demo image](/images/demo.png)](https://arz-barca.github.io/calculator/)
--- script.js
+++ script.js
@@ -1,243 +1,254 @@
 //basic functions
 
-const DECIMAL_POINTS = 6
+const DECIMAL_POINTS = 6;
 
 function add(a, b) {
-    let result = String(+a + +b);
-    if (result.includes('.')) {
-        if (result.split('.')[1].length > DECIMAL_POINTS) {
-            result = String(Number(result).toFixed(DECIMAL_POINTS))
-        }
+  let result = String(+a + +b);
+  if (result.includes(".")) {
+    if (result.split(".")[1].length > DECIMAL_POINTS) {
+      result = String(Number(result).toFixed(DECIMAL_POINTS));
     }
-    return result;
+  }
+  return result;
 }
 
 function subtract(a, b) {
-    let result = String(a - b);
-    if (result.includes('.')) {
-        if (result.split('.')[1].length > DECIMAL_POINTS) {
-            result = String(Number(result).toFixed(DECIMAL_POINTS))
-        }
+  let result = String(a - b);
+  if (result.includes(".")) {
+    if (result.split(".")[1].length > DECIMAL_POINTS) {
+      result = String(Number(result).toFixed(DECIMAL_POINTS));
     }
-    return result;
+  }
+  return result;
 }
 
 function multiply(a, b) {
-    let result = String(a * b);
-    if (result.includes('.')) {
-        if (result.split('.')[1].length > DECIMAL_POINTS) {
-            result = String(Number(result).toFixed(DECIMAL_POINTS))
-        }
+  let result = String(a * b);
+  if (result.includes(".")) {
+    if (result.split(".")[1].length > DECIMAL_POINTS) {
+      result = String(Number(result).toFixed(DECIMAL_POINTS));
     }
-    return result;
+  }
+  return result;
 }
 
 function devide(a, b) {
-    let result = String(a / b);
-    if (result.includes('.')) {
-        if (result.split('.')[1].length > DECIMAL_POINTS) {
-            result = String(Number(result).toFixed(DECIMAL_POINTS))
-        }
+  let result = String(a / b);
+  if (result.includes(".")) {
+    if (result.split(".")[1].length > DECIMAL_POINTS) {
+      result = String(Number(result).toFixed(DECIMAL_POINTS));
     }
-    return result;
+  }
+  return result;
 }
 
 function operate(a, b, operand) {
-    switch (operand.trim()) {
-        case '+':
-            return add(a, b);
-            break;
-        case '-':
-            return subtract(a, b);
-            break;
-        case '*':
-            return multiply(a, b);
-            break;
-        case '÷':
-            return devide(a, b)
-    }
+  switch (operand.trim()) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "÷":
+      return devide(a, b);
+  }
 }
 
-let displayValue = '';
-const display = document.querySelector('.display');
+let displayValue = "";
+const display = document.querySelector(".display");
 
 //adding the numbers
 
-const numberButtons = document.querySelectorAll('.button-number');
-numberButtons.forEach(button => button.addEventListener('click', updateDisplayNumber));
+const numberButtons = document.querySelectorAll(".button-number");
+numberButtons.forEach((button) =>
+  button.addEventListener("click", updateDisplayNumber)
+);
 
 function updateDisplayNumber(event) {
-    displayValue += event.target.textContent;
-    display.textContent = displayValue;
+  displayValue += event.target.textContent;
+  display.textContent = displayValue;
 }
 
 //making clear and delete functionality
 
-const deleteButton = document.querySelector('.button-delete')
-const clearButton = document.querySelector('.button-clear')
+const deleteButton = document.querySelector(".button-delete");
+const clearButton = document.querySelector(".button-clear");
 
-deleteButton.addEventListener('click', deleteDisplay);
+deleteButton.addEventListener("click", deleteDisplay);
 
 function deleteDisplay(event) {
-    let text = displayValue.split(' ')
-    let operator = ['-', '+', '*', '÷'].filter(element => element === text[1])
-    if (operator.length === 1 && text[2] === '') {
-        displayValue = displayValue.slice(0, -3);
-        display.textContent = displayValue;
-        return;
-    } else {
-        displayValue = displayValue.slice(0, -1);
-        display.textContent = displayValue;
-        return;
-    }
+  let text = displayValue.split(" ");
+  let operator = ["-", "+", "*", "÷"].filter((element) => element === text[1]);
+  if (operator.length === 1 && text[2] === "") {
+    displayValue = displayValue.slice(0, -3);
+    display.textContent = displayValue;
+    return;
+  } else {
+    displayValue = displayValue.slice(0, -1);
+    display.textContent = displayValue;
+    return;
+  }
 }
 
-clearButton.addEventListener('click', clearDisplay)
+clearButton.addEventListener("click", clearDisplay);
 
 function clearDisplay(event) {
-    displayValue = '';
-    display.textContent = displayValue;
+  displayValue = "";
+  display.textContent = displayValue;
 }
 
 // math funtions
-const functionButtons = document.querySelectorAll('.button-function');
-functionButtons.forEach(button => button.addEventListener('click', updateDisplayFunction));
+const functionButtons = document.querySelectorAll(".button-function");
+functionButtons.forEach((button) =>
+  button.addEventListener("click", updateDisplayFunction)
+);
 
 function updateDisplayFunction(event) {
-    let text = displayValue.split(' ');
-    let operator = this.textContent;
-    if (text[0] === '') {
-        return;
-    } else if (text.length === 1) {
-        displayValue += ` ${operator} `;
-        display.textContent = displayValue;
-        return;
-    } else if (text[2] === '') {
-        displayValue = displayValue.slice(0, -3) + ` ${operator} `;
-        display.textContent = displayValue;
-        return;
-    } else if (text[2] !== '') {
-        displayValue = operate(text[0], text[2], text[1]) + ` ${operator} `;
-        display.textContent = displayValue;
-        return;
-    }
+  let text = displayValue.split(" ");
+  let operator = this.textContent;
+  if (text[0] === "") {
+    return;
+  } else if (text.length === 1) {
+    displayValue += ` ${operator} `;
+    display.textContent = displayValue;
+    return;
+  } else if (text[2] === "") {
+    displayValue = displayValue.slice(0, -3) + ` ${operator} `;
+    display.textContent = displayValue;
+    return;
+  } else if (text[2] !== "") {
+    displayValue = operate(text[0], text[2], text[1]) + ` ${operator} `;
+    display.textContent = displayValue;
+    return;
+  }
 }
 
 //evaluate button
 
-const equalButton = document.querySelector('.button-equal')
-equalButton.addEventListener('click', operation)
+const equalButton = document.querySelector(".button-equal");
+equalButton.addEventListener("click", operation);
 
 function operation(event) {
-    text = displayValue.split(' ')
-    if (text[0] === '') {
-        return;
-    } else if (text.length === 1) {
-        return;
-    } else if (text[2] === '') {
-        displayValue = displayValue.slice(0, -3);
-        display.textContent = displayValue;
-        return;
-    } else if (text[2] !== '') {
-        displayValue = operate(text[0], text[2], text[1]);
-        display.textContent = displayValue;
-        return;
-    }
+  text = displayValue.split(" ");
+  if (text[0] === "") {
+    return;
+  } else if (text.length === 1) {
+    return;
+  } else if (text[2] === "") {
+    displayValue = displayValue.slice(0, -3);
+    display.textContent = displayValue;
+    return;
+  } else if (text[2] !== "") {
+    displayValue = operate(text[0], text[2], text[1]);
+    display.textContent = displayValue;
+    return;
+  }
 }
 
 // '.' button
 
-const decimalButton = document.querySelector('.button-decimal')
-decimalButton.addEventListener('click', addDecimal)
+const decimalButton = document.querySelector(".button-decimal");
+decimalButton.addEventListener("click", addDecimal);
 
 function addDecimal(event) {
-    let text = displayValue.split(' ')
-    if (text.length === 1 && !text[0].includes('.') && text[0] !== '') {
-        displayValue += '.'; 
-        display.textContent = displayValue;
-        return;
-    } else if (text.length === 3 && !text[2].includes('.')) {
-        displayValue += '.';
-        display.textContent = displayValue;
-        return ;
-    } else {
-        return;
-    }
+  let text = displayValue.split(" ");
+  if (text.length === 1 && !text[0].includes(".") && text[0] !== "") {
+    displayValue += ".";
+    display.textContent = displayValue;
+    return;
+  } else if (text.length === 3 && !text[2].includes(".")) {
+    displayValue += ".";
+    display.textContent = displayValue;
+    return;
+  } else {
+    return;
+  }
 }
 
 /*
     keyboard events
 */
 
-window.addEventListener('keydown', keyboardInputOperation)
+window.addEventListener("keydown", keyboardInputOperation);
 
 function keyboardInputOperation(event) {
-    let keyDiv = document.querySelector(`div[data-key="${event.key}"]`)
-    //console.log(event)
-    if (!keyDiv) return;
-    
+  let keyDiv = document.querySelector(`div[data-key="${event.key}"]`);
 
-    /*
+  if (!keyDiv) {
+    return;
+  }
+
+  activate(keyDiv);
+
+  /*
         number buttons
     */
 
-    if (!isNaN(Number(event.key))) {
-        displayValue += event.key;
-        display.textContent = displayValue;
-    }
+  if (!isNaN(Number(event.key))) {
+    displayValue += event.key;
+    display.textContent = displayValue;
+  }
 
-    /*
+  /*
         delete button
     */
 
-    if (event.key === 'Backspace') {
-        deleteDisplay()
-    }
+  if (event.key === "Backspace") {
+    deleteDisplay();
+  }
 
-    /*
+  /*
         function buttons
     */
 
-    if ("+-/*".includes(event.key)) {
-        let text = displayValue.split(' ');
-        let operator = event.key;
-        if (operator === "/") {
-            operator = '÷'
-        }
-        if (text[0] === '') {
-            //return;
-        } else if (text.length === 1) {
-            displayValue += ` ${operator} `;
-            display.textContent = displayValue;
-            //return;
-        } else if (text[2] === '') {
-            displayValue = displayValue.slice(0, -3) + ` ${operator} `;
-            display.textContent = displayValue;
-            //return;
-        } else if (text[2] !== '') {
-            displayValue = operate(text[0], text[2], text[1]) + ` ${operator} `;
-            display.textContent = displayValue;
-            //return;
-        }
+  if ("+-/*".includes(event.key)) {
+    let text = displayValue.split(" ");
+    let operator = event.key;
+    if (operator === "/") {
+      operator = "÷";
     }
+    if (text[0] === "") {
+      //return;
+    } else if (text.length === 1) {
+      displayValue += ` ${operator} `;
+      display.textContent = displayValue;
+      //return;
+    } else if (text[2] === "") {
+      displayValue = displayValue.slice(0, -3) + ` ${operator} `;
+      display.textContent = displayValue;
+      //return;
+    } else if (text[2] !== "") {
+      displayValue = operate(text[0], text[2], text[1]) + ` ${operator} `;
+      display.textContent = displayValue;
+      //return;
+    }
+  }
 
-    /*
+  /*
         decimal button
     */
-    
-    if (event.key === '.') {
-        addDecimal()
-    }
 
-    /*
+  if (event.key === ".") {
+    addDecimal();
+  }
+
+  /*
         enter button
     */
 
-    if (event.key === 'Enter') {
-        operation()
-    }
+  if (event.key === "Enter") {
+    operation();
+  }
 }
 
-
-
-
+// add clicking effect for keyboard events
+function activate(button) {
+  button.classList.add("active");
+  setTimeout(() => {
+    button.classList.remove("active");
+  }, 50);
+}
